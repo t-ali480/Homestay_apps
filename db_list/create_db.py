a4522b78c179d29d99333782a2025102fb5b228d
Arno <arno.tali1@icloud.com>
2024-03-24 06:00:09 -0400

    integrated expenses and paycalculator

--- db_list/create_db.py
+++ db_list/create_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Define the database name
-db_name = "personal_expenses.db"
+db_name = "March_24.db"
 
 # Connect to the database
 conn = sqlite3.connect(db_name)
@@ -11,7 +11,6 @@ cur = conn.cursor()
 # Create a table if it doesn't exist
 cur.execute(""" CREATE TABLE IF NOT EXISTS {table_name}                
                 (id INTEGER PRIMARY KEY,
-                Date DATE,
                 description TEXT,
                 category TEXT,
                 price REAL)""".format(table_name=db_name[:-3]))
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import sqlite3
-import datetime
-
-#import the databasesud
-#from /home/pudrunui/Documents/Database/expenses import expenses.db
 
 # define which database you want to use
-db_name = "/home/pudrunui/Documents/Homestay_apps/db_list/expenses.db"
-table_name = "expenses"
+db_name = "/home/hetman/projects/project_money/expenses_tracker/db_list/March_24.db"
+table_name = "March_24"
 
 conn = sqlite3.connect(db_name)    # connect to database
 cur = conn.cursor()
 
 while True:
-    print("Currently writing to database named: {table_name}".format(table_name = table_name))
+    print("Currently writing to database named: {table_name}".format(table_name=table_name))
     print("Select an option:")
     print("1.Add new expense")
     print("2.View expenses summary")
@@ -22,10 +18,9 @@ while True:
     choice = int(input())
 
     if choice == 1:
-        date = input("Enter the date of the expense (YYYY-MM-DD): ")
         description = input("Enter the description of the expense: ")
 
-        cur.execute('''SELECT DISTINCT category FROM {table_name}'''.format(table_name = table_name))
+        cur.execute('''SELECT DISTINCT category FROM {table_name}'''.format(table_name=table_name))
 
         categories = cur.fetchall()
 
@@ -36,42 +31,34 @@ while True:
               
         category_choice = int(input())
         if category_choice == len(categories) + 1:
-            category = input("Eneter the new category name: ")
+            category = input("Enter the new category name: ")
         else:
             category = categories[category_choice - 1][0]
 
         price = input("Enter the price of the expense: ")
 
-        cur.execute('''INSERT INTO {table_name} (Date, description, category, price) VALUES (?, ?, ?, ?)'''.format(table_name = table_name), (date, description, category, price))
+        cur.execute('''INSERT INTO {table_name} (description, category, price) VALUES (?, ?, ?)'''.format(table_name=table_name), (description, category, price))
 
-        
         conn.commit()
     
     elif choice == 2:
         print("Select an option:")
         print("1. View all expenses")
-        print("2. View monthly expenses by category")
+        print("2. View expenses by category")
         # may add additional options in the future
 
         view_choice = int(input())
 
         if view_choice == 1:
-            cur.execute("SELECT * FROM {table_name}".format(table_name = table_name))
+            cur.execute("SELECT * FROM {table_name}".format(table_name=table_name))
             expenses = cur.fetchall()
-            for expenses in expenses:
-                print(expenses)
+            for expense in expenses:
+                print(expense)
         elif view_choice == 2:
-            month = input("Enter the month (MM): ")
-            year = input(" Enter the year (YYYY): ")
-            cur.execute("""SELECT category, SUM(price) FROM {table_name}
-                        WHERE strftime('%m', Date) = ? AND strftime('%Y', Date) = ?
-                        GROUP BY category""".format(table_name = table_name), (month, year))
+            cur.execute("""SELECT category, SUM(price) FROM {table_name} GROUP BY category""".format(table_name=table_name))
             expenses = cur.fetchall()
             for expense in expenses:
                 print(f"Category: {expense[0]}, Total: {expense[1]}")
-        else:
-            print("exiting program")
-            exit()
 
     elif choice == 3:
         expense_id = int(input("Enter the ID of the expense you want to modify: "))
@@ -84,32 +71,24 @@ while True:
         else:
             print("Current expense details:")
             print("ID:", expense[0])
-            print("Date:", expense[1])
-            print("Description:", expense[2])
-            print("Category:", expense[3])
-            print("Price:", expense[4])
+            print("Description:", expense[1])
+            print("Category:", expense[2])
+            print("Price:", expense[3])
 
             # Get new values for modification
-            new_date = input("Enter the new date(YYYY-MM-DD) (press Enter to keep the current value): ") or expense[1]
-            new_description = input("Enter the new description (press Enter to keep the current value): ") or expense[2]
-            new_category = input("Enter the new category (press Enter to keep the current value): ") or expense[3]
-            new_price = input("Enter the new price (press Enter to keep the current value): ") or expense[4]
+            new_description = input("Enter the new description (press Enter to keep the current value): ") or expense[1]
+            new_category = input("Enter the new category (press Enter to keep the current value): ") or expense[2]
+            new_price = input("Enter the new price (press Enter to keep the current value): ") or expense[3]
 
             # Update the expense in the database
-            cur.execute(
-                """UPDATE {table_name} SET Date=?, description=?, category=?, price=? WHERE id=?""".format(
-                    table_name=table_name
-                ),
-                (new_date, new_description, new_category, new_price, expense_id),
-            )
+            cur.execute("""UPDATE {table_name} SET description=?, category=?, price=? WHERE id=?""".format(table_name=table_name), (new_description, new_category, new_price, expense_id))
             conn.commit()
             print("Expense updated successfully.")
 
     else:
         print("Exiting program")
-        exit()       
+        break
 
-    
     print("\n")
 
 
